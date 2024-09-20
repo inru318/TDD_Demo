4c9bd46f736ca216ca1e1bfbc1d4d226eb901998
王映儒 <113598015@cc.ntut.edu.tw>
2024-09-20 14:33:51 +0800

    version 5 fail Expected of multipleSingleTaskCase

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -18,4 +18,18 @@ public class test {
         crerateScheduler.addTask(createTask);
         assertEquals(5,crerateScheduler.calCostTime());
     }
+
+    @Test
+    public void multipleSingleTaskCase() throws Exception{
+        Task createTask1 = new Task(1, 5, null);
+        Task createTask2 = new Task(2, 9, null);
+        Task createTask3 = new Task(3, 7, null);
+        Task createTask4 = new Task(4, 3,null);
+        Scheduler crerateScheduler = new Scheduler();
+        crerateScheduler.addTask(createTask1);
+        crerateScheduler.addTask(createTask2);
+        crerateScheduler.addTask(createTask3);
+        crerateScheduler.addTask(createTask4);
+        assertEquals(9,crerateScheduler.calCostTime());
+    }
 }
