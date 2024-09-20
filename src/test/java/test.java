6cf15d555997f4a653cb07ca7e4f097c2d5abd4e
王映儒 <113598015@cc.ntut.edu.tw>
2024-09-20 12:51:55 +0800

    version 1 uncompleted create a task

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+
 public class test {
+    @Test
+    public void createOneTask() throws Exception {
+        Task createTask = new Task(1, 5, Arrays.asList(2, 3));
+        assertEquals(1,createTask.getId());
+        assertEquals(5,createTask.getTime());
+        assertEquals(Arrays.asList(2, 3),createTask.getDependencies());
+    }
 }
