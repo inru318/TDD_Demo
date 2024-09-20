e4234aaa8cd213345066ef1b93026d4599891df7
王映儒 <113598015@cc.ntut.edu.tw>
2024-09-20 14:45:29 +0800

    version 6 pass multipleSingleTaskCase

--- src/main/java/Scheduler.java
+++ src/main/java/Scheduler.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class Scheduler {
     private List<Task> tasks;
+    private List<Integer> roads;
 
     public Scheduler() {
         this.tasks = new ArrayList<>();
+        this.roads = new ArrayList<>();
     }
 
     public void addTask(Task task) {
@@ -13,7 +16,12 @@ public class Scheduler {
     }
 
     public int calCostTime(){
-        return tasks.getFirst().getTime();
+        for (Task task : tasks) {
+            if(task.getDependencies() == null) {
+                roads.add(task.getTime());
+            }
+        }
+        return Collections.max(roads);
     }
 
 }
